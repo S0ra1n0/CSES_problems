c44a85aa36f07005fd0b9c7cc70126072b1671fa
s0ra1n0 <148192457+S0ra1n0@users.noreply.github.com>
2025-05-28 16:30:54 +0700

    Create Exponentiation.cpp

---
+++ Exponentiation.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#define Sanic_speed ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define Ret return 0;
+#define ret return;
+#define all(x) x.begin(), x.end()
+#define el "\n";
+#define elif else if
+#define ll long long
+#define fi first
+#define se second
+#define pb push_back
+#define pops pop_back
+#define cYES cout << "YES" << "\n";
+#define cNO cout << "NO" << "\n";
+#define cYes cout << "Yes" << "\n";
+#define cNo cout << "No" << "\n";
+#define cel cout << "\n";
+#define frs(i, a, b) for(int i = a; i < b; ++i)
+#define fre(i, a, b) for(int i = a; i <= b; ++i)
+#define wh(t) while (t--)
+#define SORAI int main()
+using namespace std;
+typedef unsigned long long ull;
+ 
+const int M = 1000000007;
+ 
+ll Powering(ll a, ll b) {
+    if (b == 0) {return 1;}
+    elif (b == 1) {return a;}
+    ll temp = Powering(a, b/2);
+    temp = (temp*temp) % M;
+    if (b % 2) {
+        temp = (temp * a) % M;
+    }
+    return temp;
+}
+ 
+void solve() {
+    ll a, b;
+    cin >> a >> b;
+    if (b == 0) {
+        cout << 1 << el
+    } elif (a == 0) {
+        cout << 0 << el
+    } elif (b == 1) {
+        cout << a << el
+    } else {
+        cout << Powering(a, b) << el
+    }
+}
+ 
+SORAI {
+    Sanic_speed
+    /*
+    int n, k;
+    ll ans = 0;
+    cin >> n >> k;
+    int a[n], dp[n+1];
+    frs(i, 0, n) {dp[i] = 0; cin >> a[i];}
+    dp[n] = 0;
+    frs(i, 0, n) {
+        
+    }
+    cout << (ans%M);
+    */
+    int t;
+    cin >> t;
+    wh(t) {solve();}
+}
+/**
+     /\__/\
+    ( • 3• )
+    / >♥️<\
+**/
