a7de97e6b92b161e251d0553a81f9d23443dc254
s0ra1n0 <148192457+S0ra1n0@users.noreply.github.com>
2025-06-04 14:00:10 +0700

    Create Creating_strings_II.cpp

---
+++ Creating_strings_II.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#define Sanic_speed ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define Ret return 0;
+#define ret return;
+#define all(x) x.begin(), x.end()
+#define el "\n";
+#define elif else if
+#define ll long long
+#define fi first
+#define se second
+#define pb push_back
+#define pops pop_back
+#define cYES cout << "YES" << "\n";
+#define cNO cout << "NO" << "\n";
+#define cYes cout << "Yes" << "\n";
+#define cNo cout << "No" << "\n";
+#define frs(i, a, b) for(int i = a; i < b; ++i)
+#define fre(i, a, b) for(int i = a; i <= b; ++i)
+#define wh(t) while (t--)
+#define SORAI int main()
+using namespace std;
+typedef unsigned long long ull;
+
+const int M = 1e9 + 7;
+const int N = 1000001;
+int Facting[1000009];
+int invFacting[1000009];
+
+int Powering(int a, int b) {
+    if (b == 0) {return 1;}
+    if (b == 1) {return a;}
+    int temp = Powering(a, b/2);
+    temp = ((1LL * temp * temp) % M);
+    if (b % 2) {temp = ((1LL * temp * a) % M);}
+    return temp;
+}
+
+void GenFact() {
+    Facting[0] = 1;
+    Facting[1] = 1;
+    Facting[2] = 2;
+    fre(i, 3, N) {
+        Facting[i] = (1LL * Facting[i-1] * i % M);   
+    }
+    
+    invFacting[N] = Powering(Facting[N], M - 2);
+    for (int i = N - 1; i >= 0; --i) {
+        invFacting[i] = 1LL * invFacting[i+1] * (i+1) % M;
+    }
+}
+
+int stackingC(int a, int b) {
+    return (1LL * a * invFacting[b] % M);
+}
+
+void solve() {
+    string s;
+    cin >> s;
+    int sizer = s.size();
+    int ans = Facting[sizer];
+    vector<int> character(26, 0);
+    frs(i, 0, sizer) {
+        ++character[s[i] - 'a'];
+    }
+    frs(i, 0, 26) {
+        ans = stackingC(ans, character[i]);
+    }
+    cout << ans;
+}
+
+SORAI {
+    Sanic_speed
+    GenFact();
+    int t = 1; //cin >> t;
+    wh(t) {solve();}
+}
+/**
+     /\__/\
+    ( • 3• )
+    / >♥️<\
+**/
