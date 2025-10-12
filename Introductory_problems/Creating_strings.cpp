49ca4a46d3417367147d8261fc911ad5bd0c6500
s0ra1n0 <148192457+S0ra1n0@users.noreply.github.com>
2025-10-12 14:57:13 +0700

    Create Creating_strings.cpp

---
+++ Introductory_problems/Creating_strings.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#define Sanic_speed ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define Ret return 0;
+#define ret return;
+#define all(x) x.begin(), x.end()
+#define el "\n";
+#define elif else if
+#define ll long long
+#define fi first
+#define se second
+#define mp make_pair
+#define pf push_front
+#define pb push_back
+#define popf pop_front
+#define popb pop_back
+#define cYES cout << "YES" << "\n";
+#define cNO cout << "NO" << "\n";
+#define cYes cout << "Yes" << "\n";
+#define cNo cout << "No" << "\n";
+#define cel cout << "\n";
+#define frs(i, a, b) for(int i = a; i < b; ++i)
+#define fre(i, a, b) for(int i = a; i <= b; ++i)
+#define wh(t) while (t--)
+#define SORAI int main()
+using namespace std;
+typedef unsigned long long ull;
+ 
+void solve() {
+    string s;
+    cin >> s;
+    sort(all(s));
+    set<string> uniques;
+    do {
+        uniques.insert(s);
+    } while (next_permutation(all(s)));
+    cout << uniques.size() << el
+    for (auto i : uniques) {cout << i << el}
+}
+ 
+SORAI {
+    Sanic_speed
+    int t = 1; //cin >> t;
+    wh(t) {solve();}
+}
+/**
+     /\__/\
+    ( • 3• )
+    / >♥️<\
+**/
